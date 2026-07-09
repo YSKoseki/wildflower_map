6193426bed0d399c0e5506f1d7ecac06f6c96263
Yusuke Koseki / 小関 右介 <104213744+YSKoseki@users.noreply.github.com>
2026-07-09 21:54:01 +0900

    Store marker stroke colors in plant data

--- plants.js
+++ plants.js
@@ -3,3 +3,22 @@ window.FAMILIES = {
         name: "キク科"
     }
 };
+
+window.PLANTS = [
+    {
+        id: "onitabirako",
+        species: "オニタビラコ",
+        family: "キク科",
+        familyId: "kiku",
+        color: "#ffa502",
+        strokeColor: "#ffffff"
+    },
+    {
+        id: "tanpopo",
+        species: "タンポポ",
+        family: "キク科",
+        familyId: "kiku",
+        color: "#ffec3d",
+        strokeColor: "#a0a0a0"
+    }
+];
