05935bbd0792ecc2fb30ede3cab988894fbe7ba2
Yusuke Koseki / 小関 右介 <104213744+YSKoseki@users.noreply.github.com>
2026-07-09 23:54:51 +0900

    Refactor family definitions using shared constants

--- plants.js
+++ plants.js
@@ -1,24 +1,90 @@
+window.FAMILY = {
+    KIKU: "kiku",
+    NADESHIKO: "nadeshiko",
+    KATABAMI: "katabami",
+    OOBAKO: "oobako",
+    KIJIKAKUSHI: "kijikakushi",
+    AKANE: "akane",
+    ABURANA: "aburana",
+    AYAME: "ayame",
+    KIKYOU: "kikyou",
+    KESHI: "keshi",
+    TADE: "tade",
+    DOKUDAMI: "dokudami",
+    NASU: "nasu",
+    HAEDOKUSOU: "haedokusou",
+    HIMAWARI: "himawari",
+    FUUROSOU: "fuurosou",
+    MURASAKI: "murasaki"
+};
+
 window.FAMILIES = {
-    kiku: {
+    [FAMILY.KIKU]: {
         name: "キク科"
+    },
+    [FAMILY.NADESHIKO]: {
+        name: "ナデシコ科"
+    },
+    [FAMILY.KATABAMI]: {
+        name: "カタバミ科"
+    },
+    [FAMILY.OOBAKO]: {
+        name: "オオバコ科"
+    },
+    [FAMILY.KIJIKAKUSHI]: {
+        name: "キジカクシ科"
+    },
+    [FAMILY.AKANE]: {
+        name: "アカネ科"
+    },
+    [FAMILY.ABURANA]: {
+        name: "アブラナ科"
+    },
+    [FAMILY.AYAME]: {
+        name: "アヤメ科"
+    },
+    [FAMILY.KIKYOU]: {
+        name: "キキョウ科"
+    },
+    [FAMILY.KESHI]: {
+        name: "ケシ科"
+    },
+    [FAMILY.TADE]: {
+        name: "タデ科"
+    },
+    [FAMILY.DOKUDAMI]: {
+        name: "ドクダミ科"
+    },
+    [FAMILY.NASU]: {
+        name: "ナス科"
+    },
+    [FAMILY.HAEDOKUSOU]: {
+        name: "ハエドクソウ科"
+    },
+    [FAMILY.HIMAWARI]: {
+        name: "ヒマワリ科"
+    },
+    [FAMILY.FUUROSOU]: {
+        name: "フウロソウ科"
+    },
+    [FAMILY.MURASAKI]: {
+        name: "ムラサキ科"
     }
 };
 
 window.PLANTS = [
     {
+        familyId: FAMILY.KIKU,
         id: "onitabirako",
         species: "オニタビラコ",
-        family: "キク科",
-        familyId: "kiku",
         color: "#ffa502",
         strokeColor: "#ffffff"
     },
     {
+        familyId: FAMILY.KIKU,
         id: "tanpopo",
         species: "タンポポ",
-        family: "キク科",
-        familyId: "kiku",
         color: "#ffec3d",
-        strokeColor: "#a0a0a0"
+        strokeColor: "#666666"
     }
 ];
